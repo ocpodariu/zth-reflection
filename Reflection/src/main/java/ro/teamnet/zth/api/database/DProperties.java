f4794a6736a44f4c82fc19f75f4359b7a4230a4a
Ovidiu PODARIU <ocpodariu@yahoo.com>
2015-04-28 20:38:35 +0300

    Define database connection properties

---
+++ Reflection/src/main/java/ro/teamnet/zth/api/database/DProperties.java
@@ -0,0 +1,16 @@
+package ro.teamnet.zth.api.database;
+
+/**
+ * Author: Ovidiu
+ * Date:   4/28/2015
+ */
+public interface DProperties {
+
+    String IP       = "127.0.0.1";
+    String PORT     = "3306";
+    String SCHEMA   = "ZTH_11";
+    String USER     = "root";
+    String PASS     = "";
+    String DRIVER_CLASS = "com.mysql.jdbc.Driver";
+
+}
