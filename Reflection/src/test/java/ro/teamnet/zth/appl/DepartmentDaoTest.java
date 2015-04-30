03ab13fc9edb0b584020e50ffc976f0683a6b33b
Ovidiu PODARIU <ocpodariu@yahoo.com>
2015-04-30 17:42:26 +0300

    Add unit tests for Department DAO

---
+++ Reflection/src/test/java/ro/teamnet/zth/appl/DepartmentDaoTest.java
@@ -0,0 +1,72 @@
+package ro.teamnet.zth.appl;
+
+import org.junit.Assert;
+import org.junit.Test;
+import ro.teamnet.zth.api.em.EntityManager;
+import ro.teamnet.zth.api.em.EntityManagerImpl;
+import ro.teamnet.zth.appl.dao.DepartmentDao;
+import ro.teamnet.zth.appl.domain.Department;
+
+import java.util.List;
+
+/**
+ * Author: Ovidiu
+ * Date:   4/30/2015
+ */
+public class DepartmentDaoTest {
+
+    @Test
+    public void testFindById() {
+        Long departmentId = new Long(10);
+        Department department = new Department();
+        department.setId(departmentId);
+        department.setDepartmentName("Administration");
+        department.setLocation(new Long(1700));
+
+        DepartmentDao departmentDao = new DepartmentDao();
+        Assert.assertEquals("Error retrieving department record!", department, departmentDao.findById(departmentId));
+    }
+
+    @Test
+    public void testFindAll() {
+        int expectedNumberOfDepartments = 27;
+
+        DepartmentDao departmentDao = new DepartmentDao();
+        Assert.assertEquals("Number of results doesn't match!", expectedNumberOfDepartments, departmentDao.findAll().size());
+    }
+
+    @Test
+    public void testInsert() {
+        Department department = new Department();
+        department.setDepartmentName("ZeroToHero");
+        department.setLocation(new Long(1700));
+
+        DepartmentDao departmentDao = new DepartmentDao();
+        Assert.assertEquals("Error inserting record!", department.getDepartmentName(), departmentDao.insert(department).getDepartmentName());
+    }
+
+    @Test
+    public void testUpdate() {
+        Department department = new Department();
+        department.setId(new Long(271));
+        department.setDepartmentName("ZeroToHero-JAVA");
+        department.setLocation(new Long(1700));
+
+        DepartmentDao departmentDao = new DepartmentDao();
+        Assert.assertEquals("Error updating record!", department, departmentDao.update(department));
+    }
+
+    @Test
+    public void testDelete() {
+        Department department = new Department();
+        department.setId(new Long(271));
+        department.setDepartmentName("ZeroToHero-JAVA");
+        department.setLocation(new Long(1700));
+
+        DepartmentDao departmentDao = new DepartmentDao();
+        departmentDao.delete(department);
+
+        Assert.assertNull("Record not deleted!", departmentDao.findById(new Long(271)));
+    }
+
+}
