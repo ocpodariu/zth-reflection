8eee690dcd4a6208ac71dcfd295f21e707ddf06f
Ovidiu PODARIU <ocpodariu@yahoo.com>
2015-04-30 15:20:16 +0300

    Implement delete()

--- Reflection/src/main/java/ro/teamnet/zth/api/em/EntityManagerImpl.java
+++ Reflection/src/main/java/ro/teamnet/zth/api/em/EntityManagerImpl.java
@@ -31,23 +31,23 @@ public class EntityManagerImpl implements EntityManager {
             // for (ColumnInfo column : columns) {}
 
             // Find primary key column i.e. ID column
-            String id_column = null;
+            String idColumn = null;
             for (ColumnInfo column : columns) {
                 if (column.isId()) {
-                    id_column = column.getDbName();
+                    idColumn = column.getDbName();
                     break;
                 }
             }
 
             // Check if entity has a primary key; otherwise, early exit
-            if (id_column == null) {
+            if (idColumn == null) {
                 System.out.println("ERROR! Entity doesn't have a primary key.");
                 return null;
             }
 
             // Define query condition (find by ID)
             Condition condition = new Condition();
-            condition.setColumnName(id_column);
+            condition.setColumnName(idColumn);
             condition.setValue(id);
 
             // Create the actual query
@@ -60,7 +60,7 @@ public class EntityManagerImpl implements EntityManager {
             String sqlQuery = queryBuilder.createQuery();
 
             // Execute the query and store the results in a new instance of class
-            try (Statement stmt = DBManager.getConnection().createStatement();
+            try (Statement stmt = conn.createStatement();
                  ResultSet result = stmt.executeQuery(sqlQuery)) {
                 result.first();
 
@@ -127,7 +127,7 @@ public class EntityManagerImpl implements EntityManager {
             String sqlQuery = queryBuilder.createQuery();
 
             // Execute the query and store results in a list
-            try (Statement stmt = DBManager.getConnection().createStatement();
+            try (Statement stmt = conn.createStatement();
                  ResultSet results = stmt.executeQuery(sqlQuery)) {
 
                 // Navigate result set
@@ -314,6 +314,58 @@ public class EntityManagerImpl implements EntityManager {
 
     @Override
     public void delete(Object entity) {
+        // Create a connection to the Database
+        try (Connection conn = DBManager.getConnection()) {
+            // Get table name
+            String tableName = EntityUtils.getTableName(entity.getClass());
 
+            // Get table columns
+            List<ColumnInfo> columns = EntityUtils.getColumns(entity.getClass());
+
+            // Set column values to the current values of the entity
+            try {
+                Field field;
+                for (ColumnInfo column : columns) {
+                    field = entity.getClass().getDeclaredField(column.getColumnName());
+                    column.setValue(field.get(entity));
+                }
+            } catch (NoSuchFieldException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+
+            // Get ID column name and value of record to be updated
+            String idColumn = null;
+            Object idValue = null;
+
+            for (ColumnInfo column : columns)
+                if (column.isId()) {
+                    idColumn = column.getDbName();
+                    idValue = column.getValue();
+                    break;
+                }
+
+            // Define query condition (find by ID and update)
+            Condition condition = new Condition();
+            condition.setColumnName(idColumn);
+            condition.setValue(idValue);
+
+            // Create the actual query
+            QueryBuilder queryBuilder = new QueryBuilder();
+            queryBuilder.setTableName(tableName)
+                    .setQueryType(QueryType.UPDATE)
+                    .addQueryColumns(columns)
+                    .addCondition(condition);
+
+            String sqlQuery = queryBuilder.createQuery();
+
+            conn.createStatement().executeQuery(sqlQuery);
+
+            // Commit ???
+            // conn.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
